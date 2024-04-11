aad5db0593d0a33cbe521652432c88e02393cd1d
Patriach Cain <youanybluesky10@gmail.com>
2024-04-11 21:05:12 +0800

    revised the error popup

--- forms/script.js
+++ forms/script.js
@@ -1,18 +1,17 @@
 const password1 = document.querySelector('#user_password');
 const password2 = document.querySelector('#user_password_2');
 const form = document.querySelector('form');
-const passwordDiv = document.querySelector('.password');
+const popUp = document.querySelector('.popUp');
+let timeout = null;
 
 form.addEventListener('submit', (event) => {
   if (password1.value !== password2.value || password1.value === '' || password2.value === '')  {
     event.preventDefault(); 
-    const errorMessage = document.createElement('span');
-    errorMessage.classList.add('popUp');
-    errorMessage.textContent = '*Password do not match!'
-    passwordDiv.appendChild(errorMessage);
-    setTimeout(()=> {
-      errorMessage.style.display = 'none';
-    }, 2000)
+    popUp.style.display = 'inline';
   }
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(()=> {
+    popUp.style.display = 'none';
+  }, 2000)
 });
 
